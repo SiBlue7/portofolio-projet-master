d9d5e08dc8804fd0190d1d206db01f27b0908b5f
SiBlue <enzo20112002@gmail.com>
2026-04-17 15:46:10 +0200

    feat (config Prisma): first db migration

---
+++ prisma/migrations/20260417133620_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateEnum
+CREATE TYPE "ProjectStatus" AS ENUM ('DRAFT', 'IN_PROGRESS', 'COMPLETED', 'ARCHIVED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "shortDescription" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "githubUrl" TEXT,
+    "productionUrl" TEXT,
+    "preproductionUrl" TEXT,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "isMaintained" BOOLEAN NOT NULL DEFAULT true,
+    "status" "ProjectStatus" NOT NULL DEFAULT 'DRAFT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
